aa7ab679d831f8850061dde68645af5622cecd69
dandenney <dd.subscriptions@gmail.com>
2017-08-02 11:43:12 -0400

    refactor: Move markup for a book into Book component

--- src/Book.js
+++ src/Book.js
@@ -5,10 +5,46 @@ class Book extends Component {
     const { filteredBooks } = this.props;
 
     return (
-      <div>
-        {filteredBooks.map(filteredBook => (
-          <div key={filteredBook.id}>{filteredBook.title}</div>
-        ))}
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {filteredBooks.map(filteredBook => (
+            <li key={filteredBook.id}>
+              <div className="book">
+                <div className="book-top">
+                  <div
+                    className="book-cover"
+                    style={{
+                      width: 128,
+                      height: 193,
+                      backgroundImage: `url(${filteredBook.imageLinks.thumbnail})`
+                    }}
+                  />
+                  <div className="book-shelf-changer">
+                    <select>
+                      <option value="none" disabled>
+                        Move to...
+                      </option>
+                      <option value="currentlyReading">
+                        Currently Reading
+                      </option>
+                      <option value="wantToRead">
+                        Want to Read
+                      </option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="book-title">
+                  {filteredBook.title}
+                </div>
+                <div className="book-authors">
+                  {filteredBook.authors.join(", ")}
+                </div>
+              </div>
+            </li>
+          ))}
+        </ol>
       </div>
     );
   }
